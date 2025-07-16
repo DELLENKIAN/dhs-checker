8db551ba290f791b4abfa268f04d2bd6c2a29b04
Dellen Kian Govender <dg.removedebt@gmail.com>
2025-07-16 15:19:55 +0200

    Expose Flask on 0.0.0.0

--- main.py
+++ main.py
@@ -49,5 +49,7 @@ def upload_file():
     return render_template_string(HTML_FORM)
 
 if __name__ == '__main__':
-    app.run(debug=False, host="0.0.0.0", port=8000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
+
+
 
